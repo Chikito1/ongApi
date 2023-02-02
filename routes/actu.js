3c1477b2699f05b37bb610e4ded6a6e4d0eeb476
Ahmed Binate <hamedbinate401@gmail.com>
2023-02-02 22:00:45 +0000

    Router update

--- routes/actu.js
+++ routes/actu.js
@@ -4,9 +4,7 @@ const actucltr = require('../controllers/actu')
 
 const upload = require('../middlewares/uploader')
 
-router.route('/')
-    .get(actucltr.readAllactu)
-    .post(upload, actucltr.createActu)
+router.route('/').get(actucltr.readAllactu).post(upload, actucltr.createActu)
 
 router.route('/:id').get(actucltr.readOneactu)
 
