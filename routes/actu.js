225dad93adcd3b1152071ebf73e9ada7f6177f0e
Bgk Injector SqLi <borisbob91@gmail.com>
2023-02-26 20:33:08 +0000

    feat: actu route controller done!

--- routes/actu.js
+++ routes/actu.js
@@ -3,14 +3,15 @@ var router = express.Router();
 const actucltr = require('../controllers/actu')
 
 const upload = require('../middlewares/uploader').fields([{name:"image_url", maxCount:1}]);
+const imgUploadHandler = require('../middlewares/img.upload.hanlder')
 
 router.route('/')
     .get(actucltr.readAllactu)
-    .post(upload, actucltr.createActu)
+    .post(upload,imgUploadHandler, actucltr.createActu)
 
 router.route('/:id')
     .get(actucltr.readOneactu)
-    .put(upload, actucltr.updateActu)
+    .put(upload, imgUploadHandler, actucltr.updateActu)
     .delete(actucltr.deleteActu)
 
 
